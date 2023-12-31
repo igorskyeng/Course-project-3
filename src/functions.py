b1befacd4e29bb112d6da11db8bddf3e2f69bae0
Igor Barbur <Igor.barbur@yandex.ru>
2023-12-31 21:45:02 +0800

    fix functions.pyonds and fix main.py

--- src/functions.py
+++ src/functions.py
@@ -3,14 +3,15 @@ import json
 import datetime
 
 
-def load_operations():
+def load_operations(file='operations.json'):
     """
     Загружает список банковсих операций из файла "operations.json"
+    :file: Файл для чтения
     :return: Возвращает список банковсих операций
     """
     list_of_operations = ''
 
-    with open('operations.json', 'r', encoding='UTF=8') as file:
+    with open(file, 'r', encoding='UTF=8') as file:
         for line in file:
             list_of_operations += line
 
@@ -19,15 +20,17 @@ def load_operations():
     return list_of_operations
 
 
-def after_of_operations():
+def after_of_operations(operations,number_of_operation=-5):
     """
     Получает список банковсих операций из функции "load_operations()"
-    :return: Возвращает список 5 последних операций
+    :operations: Данные по банковсой операции
+    :number_of_operation: Количесвто операцый для вывода
+    :return: Возвращает список последних операций
     """
     list_of_operations = []
     last_5_list_of_operations = []
 
-    for operation in load_operations():
+    for operation in operations:
         if "date" not in operation:
             continue
 
@@ -40,16 +43,23 @@ def after_of_operations():
             list_of_operations.append(date_of_operation)
 
         list_of_operations = sorted(list_of_operations)
-        last_5_list_of_operations = list_of_operations[-5:]
+        last_5_list_of_operations = list_of_operations[number_of_operation:]
         last_5_list_of_operations.reverse()
 
     return last_5_list_of_operations
 
 
 def slices_by_operation(check_and_card):
+    """
+    Получает список банковсих операций из функции "load_operations()"
+    :check_and_card: Номер и банк карты
+    :return: Возвращает замаскированный номер карты и ее банк
+    """
+    # Записывает замаскированный номер карты
     to_operation = (check_and_card[-16:-12] + " " + check_and_card[-12:-10] +
                     '** **** ' + check_and_card[-4:])
 
+    # Добавляет к записанному замаскированному номеру карты ее банк
     if "Maestro" in check_and_card:
         to_operation = "Maestro " + to_operation
 
--- src/main.py
+++ src/main.py
@@ -4,19 +4,39 @@ from src.functions import load_operations
 
 from src.functions import slices_by_operation
 
-for date in after_of_operations():
-    for operation in load_operations():
-        if "date" not in operation:
-            continue
 
-        elif str(date) in operation["date"]:
-            if "from" not in operation:
-                print(f'{date.strftime("%d.%m.%Y")} {operation["description"]}\n'
-                      f'{slices_by_operation(operation["to"])}\n{operation["operationAmount"]["amount"]} '
-                      f'{operation["operationAmount"]["currency"]["name"]}\n')
+def input_operation(file='operations.json'):
+    """
+    Выводит пользователю 5 псоедних выполненных банковских операций
+    """
 
-            else:
-                from_operation = operation["from"][-16:-12]
-                print(f'{date.strftime("%d.%m.%Y")} {operation["description"]}\n'
-                      f'{slices_by_operation(operation["from"])} -> {slices_by_operation(operation["to"])}\n'
-                      f'{operation["operationAmount"]["amount"]} {operation["operationAmount"]["currency"]["name"]}\n')
+    # Скрывает скрип функции от запуска при импорте в "test_main.py"
+    if __name__ == "__main__":
+
+        # Итерирует даты 5 последних банковских операций
+        for date in after_of_operations(load_operations(file)):
+
+            # Итерирует все банковские операции
+            for operation in load_operations(file):
+
+                # Пропускает банковские операции при отсутствии даты
+                if "date" not in operation:
+                    continue
+
+                elif str(date) in operation["date"]:
+
+                    # Вывод открытия банковской операции
+                    if "from" not in operation:
+                        print(f'{date.strftime("%d.%m.%Y")} {operation["description"]}\n'
+                              f'{slices_by_operation(operation["to"])}\n{operation["operationAmount"]["amount"]} '
+                              f'{operation["operationAmount"]["currency"]["name"]}\n')
+
+                    # Вывод перевода банковской операции
+                    else:
+                        print(f'{date.strftime("%d.%m.%Y")} {operation["description"]}\n'
+                              f'{slices_by_operation(operation["from"])} -> {slices_by_operation(operation["to"])}\n'
+                              f'{operation["operationAmount"]["amount"]} '
+                              f'{operation["operationAmount"]["currency"]["name"]}\n')
+
+
+input_operation()
